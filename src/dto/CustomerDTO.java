293b29659dc828ad9dd02187707ba30ffe9da02f
lucas <moron.lucas@gmail.com>
2019-09-30 22:40:25 -0300

    agregue relacion con payment y metodos get-add-remove

--- src/dto/CustomerDTO.java
+++ src/dto/CustomerDTO.java
@@ -19,21 +19,23 @@ public class CustomerDTO extends PersonDTO
 	private int _id;
 	private String _state;
 
-	@OneToOne
+	@OneToOne(cascade = CascadeType.ALL)
 	private DebtDTO _debt;
-	@OneToOne
+	
+	@OneToOne(cascade = CascadeType.ALL)
 	private BagDTO _bag;
 
 	@OneToMany (mappedBy = "_customer", cascade = CascadeType.ALL,orphanRemoval = true)
 	private List<TurnDTO> _turns = new ArrayList<>();
 
+	@OneToMany (mappedBy = "_customerp", cascade = CascadeType.ALL,orphanRemoval = true)
+	private List<PaymentDTO> _payments = new ArrayList<>();
 	
 	public CustomerDTO(String name, String surname, String age, String mail, String numberPhone,
-					String state, DebtDTO debt, BagDTO bag)
+					String state)
 	{
 		super(name, surname, age, mail, numberPhone);
 		_state = state;
-		_debt = debt;
 	}
 	
 	public CustomerDTO() {}
@@ -66,5 +68,39 @@ public class CustomerDTO extends PersonDTO
 	public void setDebt(DebtDTO debt) 
 	{
 		_debt = debt;
+	}
+	
+	public List<TurnDTO> getTurns() 
+	{
+		return _turns;
+	}
+	
+	public void addTurn(TurnDTO t)
+	{
+		this._turns.add(t);
+		t.setCustomer(this);
+	}
+
+	public void removeTurn(TurnDTO t)
+	{
+		this._turns.remove(t);
+		t.setCustomer(this);
+	}
+	
+	public List<PaymentDTO> getPayments() 
+	{
+		return _payments;
+	}
+	
+	public void addPayment(PaymentDTO p)
+	{
+		this._payments.add(p);
+		p.setCustomer(this);
+	}
+
+	public void removePayment(PaymentDTO p)
+	{
+		this._payments.remove(p);
+		p.setCustomer(this);
 	}
 }
