27ccf247ecbe9edacb06d3c39fbb090c1880876a
Mauro Zarza <mauro.mdz798@gmail.com>
2019-10-03 17:09:53 -0300

    recivo de prueba 1

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,38 +1,26 @@
 package main;
 
+import java.time.LocalDate;
+
 import dto.BranchOfficeDTO;
 import dto.EmployeeDTO;
 import dto.ServiceDTO;
+import dto.TurnDTO;
 import dto.WorkdayDTO;
 import persistencia.dao.implementacion.BranchOfficeJPA;
 import persistencia.dao.implementacion.EmployeeJPA;
 import persistencia.dao.implementacion.ServiceJPA;
 import persistencia.dao.implementacion.WorkdayJPA;
+import receipts.ReservationReceipt;
 
 public class Main 
 {
 
 	public static void main(String[] args) 
 	{
-		BranchOfficeDTO b1 = new BranchOfficeDTO("Breikin", "Mitre 123", "San miguel", "Bs As", "Argentina");
-		BranchOfficeJPA bDAO= new BranchOfficeJPA();
 		
-		WorkdayDTO d1 = new WorkdayDTO("Lunes", "0800hs", "1200hs"); //lista de dias
-		WorkdayJPA dDAO = new WorkdayJPA();
-		
-		ServiceDTO s1 = new ServiceDTO("Corte de pelo", 45, 300); //lista de servicios
-		ServiceJPA sDAO = new ServiceJPA();
-		
-		EmployeeDTO e1 = new EmployeeDTO("Lucas", "Martin", "20", "mail@gmail.com", "46678393", "Recepcionista",
-									"alta", "lmartin", "123456s");
-		EmployeeJPA eDAO = new EmployeeJPA();
-		
-		bDAO.create(b1);
-		bDAO.update(b1);	
-		dDAO.create(d1);
-		sDAO.create(s1);
-		eDAO.create(e1);
-		bDAO.close();
+
+
 		
 	
 		
