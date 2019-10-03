1b43b11a7bfa56254f30952410a2d74a54e36b35
Gonzalo Ortelli <gonzaloortelli@gmail.com>
2019-10-02 21:36:41 -0300

    Se empezo con ControllerTakeTurn.

--- src/modelo/Turn.java
+++ src/modelo/Turn.java
@@ -45,8 +45,8 @@ public class Turn
 		return _turn.all();
 	}
 	
-	//Creacion de un nuevo turno en base a los datos proporcionados.
-	public void creationOfTurn(LocalDateTime reservation_day, LocalDateTime service_day) throws Exception 
+	//Creacion de un nuevo turno en base a la hora del servicio.
+	public void creationOfTurn(LocalDateTime service_day) throws Exception 
 	{
 		TurnDTO new_turn = new TurnDTO(LocalDateTime.now(), service_day, "Vigente");
 		createTurn(new_turn);
--- src/presentacion/controlador/ControllerTakeTurn.java
+++ src/presentacion/controlador/ControllerTakeTurn.java
@@ -1,7 +1,37 @@
 package presentacion.controlador;
 
-public class ControllerTakeTurn 
-{
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+import dto.ProfessionalDTO;
+import dto.ServiceDTO;
+import dto.WorkdayDTO;
+import modelo.Professional;
+import modelo.Service;
+import modelo.Turn;
+import modelo.Workday;
+
+public class ControllerTakeTurn implements ActionListener // VERSION SIN VENTANAS
+{	
+	private Professional _professional_model;
+	private Service _service_model;
+	private Workday _workday_model;
+	private Turn _turnl_model;
+	private List<ServiceDTO> _services;
+	private List<ProfessionalDTO> _professionals_with_selected_service;
+	private List<WorkdayDTO> _workdays_of_selected_proffesional;
+	
+	public ControllerTakeTurn(Professional professional, Service service, Workday workday, Turn turn) // FALTA MAIN WINDOW
+	{
+		_professional_model = professional;
+		_service_model = service;
+		_workday_model = workday;
+		_turnl_model = turn;
+		
+		_services = null;
+		_professionals_with_selected_service = null;
+		_workdays_of_selected_proffesional = null;
+	}
 	
 	public void fillServices() 
 	{
@@ -20,6 +50,12 @@ public class ControllerTakeTurn
 	
 	public void fillSchedules() 
 	{
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 }
