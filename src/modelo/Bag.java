ca5130017e6b83e72924161d8ada9aca5b1b6590
Ramos Boris <borisramos_77@hotmail.com>
2019-10-03 11:48:29 -0300

    Agregado de metodo en addPointsPay en Package modelo Bag

--- src/modelo/Bag.java
+++ src/modelo/Bag.java
@@ -1,7 +1,9 @@
 package modelo;
 
 import java.util.List;
+
 import dto.BagDTO;
+import dto.CustomerDTO;
 import persistencia.dao.implementacion.BagJPA;
 import persistencia.dao.interfaz.BagDAO;
 
@@ -32,22 +34,36 @@ public class Bag
 	}
 	
 	//Retorno de una cartera por ID
-		public void getPaymentID(long bagID) throws Exception 
-		{
-			_bag.byId(bagID);
-		}
+	public void getPaymentID(long bagID) throws Exception 
+	{
+		_bag.byId(bagID);
+	}
 	
 	//Lista de todos los pagos (DENTRO DEL SISTEMA)
 	public List<BagDTO> obtainPayments() throws Exception 
 	{
 		return _bag.all();
 	}
+	
+	// Suma puntos por cada pago. 
+	public void addPointsPay(CustomerDTO customerBag ,int payment)
+	{
+		/** RELACION --->  500 PESOS = 10 PUNTOS */ // Prodrian ser otras intancias de Bag
+		int pointForPay = 10;
+		int everyAmount = 500;
+		if(payment >= 500)
+		{
+			int pointsAccumulated = customerBag.getBag().getPoints() + (payment/everyAmount)*pointForPay;
+			customerBag.getBag().setPoints(pointsAccumulated);
+		}
+	}
 
 	// Convertidor de puntos en pesos Argentinos $
-	public int convetPointsMoney(BagDTO customerBag)
+	public int convetPointsMoney(CustomerDTO customerBag)
 	{
-		int costPoint = 2; /** RELACION --->  1 PUNTO = 2 PESOS */
-		int moneyForPoints = costPoint * customerBag.getPoints();
+		/** RELACION --->  1 PUNTO = 2 PESOS */ // Prodria ser otra intancia de Bag
+		int costPoint = 2; 
+		int moneyForPoints = costPoint * customerBag.getBag().getPoints();
 		return moneyForPoints;
 	}
 	
