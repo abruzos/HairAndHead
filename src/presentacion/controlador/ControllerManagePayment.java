9d9c3caab0c2e91f68417e8a254eca12d614c2a2
Ramos Boris <borisramos_77@hotmail.com>
2019-10-04 11:05:13 -0300

    Agregado de metodos dentro Controlador Gestion de pagos

--- src/presentacion/controlador/ControllerManagePayment.java
+++ src/presentacion/controlador/ControllerManagePayment.java
@@ -2,26 +2,53 @@ package presentacion.controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import javax.swing.JOptionPane;
 import modelo.Customer;
 import presentacion.vista.ManagePaymentWindow;
 
 public class ControllerManagePayment implements ActionListener{
 	
-	private ManagePaymentWindow _view;
+	private ManagePaymentWindow _viewManagePay;
 	private Customer _customer;
 	
 	public ControllerManagePayment(ManagePaymentWindow view, Customer customer)
 	{
-		_view = view;
+		_viewManagePay = view;
 		_customer = customer;
-		_view.selectPayment().addActionListener(select -> choosePaymentWindow(select));
+		loadCBMwayToPay();
+		_viewManagePay.selectPayment().addActionListener(select -> choosePaymentWindow(select));
 	}
 	
-	// 
+	// Carga el comboBox con las formas de pago.
+	/** TENDRIA QUE SER UNA LISTA DE ESTADOS O ESTAR EN BDD */
+	private void loadCBMwayToPay() 
+	{
+		// List<PaymentStatusDTO> PaymentStatus = BranchOffice.getPaymentStatus();
+		// for (int status : PaymentStatus) {}
+		_viewManagePay.getCmbSelectionPay().addItem("En Efectivo");
+		_viewManagePay.getCmbSelectionPay().addItem("Con Puntos");
+		_viewManagePay.getCmbSelectionPay().addItem("Con Deuda");	
+	}
+
+	// Elige la ventada de pago
 	private void choosePaymentWindow(ActionEvent select)
 	{
-		//...
+		if(_viewManagePay.getCmbSelectionPay().getSelectedItem().equals("En Efectivo"))
+		{
+			
+		}
+		if(_viewManagePay.getCmbSelectionPay().getSelectedItem().equals("Con Puntos"))
+		{
+				
+		}
+		if (_viewManagePay.getCmbSelectionPay().getSelectedItem().equals("Con Deuda"))
+		{
+					
+		}
+		else
+		{
+			JOptionPane.showMessageDialog(null,"ADVERTENCIA!! No hay seleccion de ninguna Forma de Pago","Mensaje", JOptionPane.INFORMATION_MESSAGE);
+		}
 	}
 	
 	@Override
