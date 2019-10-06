8465903407b0785c61ebd96d6d3ebdf226625b4c
Ramos Boris <borisramos_77@hotmail.com>
2019-10-06 11:03:26 -0300

    Incorporacion de clases validaciones para las vistas (a definir)

---
+++ src/modelo/validacion/PaymentValidation.java
@@ -0,0 +1,13 @@
+package modelo.validacion;
+
+/**
+ * PARA DECLARAR QUE TIPO DE DATO O FORMATO QUE RECIVE 
+ * DESDE LA VISTA LOS CONTROLADORES DE PAGO
+ */
+public class PaymentValidation
+{
+	
+	// CONSTRUCTOR VACIO
+	public PaymentValidation(){}
+	
+}
