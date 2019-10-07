d365aa7042a8116b51e9bfb739a0ee3e39deca7d
poccock <moron.lucas@gmail.com>
2019-10-06 22:12:46 -0300

    vistas cash debt points

--- src/presentacion/vista/PaymentDebtWindow.java
+++ src/presentacion/vista/PaymentDebtWindow.java
@@ -1,5 +1,127 @@
 package presentacion.vista;
 
-public class PaymentDebtWindow {
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class PaymentDebtWindow 
+{
+	private JFrame frmPaymentDebt;
+	private JPanel pnlPaymentDebt;
+	private JLabel lblServiceCost;
+	private JLabel lblPendingDebt;
+	private JLabel lblCurrentDebt;
+	private JTextField txtServiceCost;
+	private JTextField txtPendingDebt;
+	private JTextField txtCurrentDebt;
+	private JButton btnAccept;
+	private JButton btnCancel;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PaymentDebtWindow window = new PaymentDebtWindow();
+					window.frmPaymentDebt.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PaymentDebtWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() 
+	{
+		frmPaymentDebt = new JFrame();
+		frmPaymentDebt.setResizable(false);
+		frmPaymentDebt.setTitle("Payment Debt");
+		frmPaymentDebt.setBounds(100, 100, 532, 300);
+		frmPaymentDebt.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmPaymentDebt.getContentPane().setLayout(null);
+		
+		pnlPaymentDebt = new JPanel();
+		pnlPaymentDebt.setBounds(10, 11, 506, 249);
+		frmPaymentDebt.getContentPane().add(pnlPaymentDebt);
+		pnlPaymentDebt.setLayout(null);
+		
+		lblServiceCost = new JLabel("Costo de servicio");
+		lblServiceCost.setBounds(20, 23, 110, 23);
+		pnlPaymentDebt.add(lblServiceCost);
+				
+		lblPendingDebt = new JLabel("Deuda pendiente");
+		lblPendingDebt.setBounds(20, 60, 110, 23);
+		pnlPaymentDebt.add(lblPendingDebt);
+		
+		lblCurrentDebt = new JLabel("Deuda actual");
+		lblCurrentDebt.setBounds(20, 101, 110, 23);
+		pnlPaymentDebt.add(lblCurrentDebt);
+		
+		txtServiceCost = new JTextField();
+		txtServiceCost.setColumns(10);
+		txtServiceCost.setBounds(189, 24, 110, 20);
+		pnlPaymentDebt.add(txtServiceCost);
+		
+		txtPendingDebt = new JTextField();
+		txtPendingDebt.setColumns(10);
+		txtPendingDebt.setBounds(189, 61, 110, 20);
+		pnlPaymentDebt.add(txtPendingDebt);
+		
+		txtCurrentDebt = new JTextField();
+		txtCurrentDebt.setColumns(10);
+		txtCurrentDebt.setBounds(189, 102, 110, 20);
+		pnlPaymentDebt.add(txtCurrentDebt);
+				
+		btnAccept = new JButton("Acepar");
+		btnAccept.setBounds(96, 178, 89, 23);
+		pnlPaymentDebt.add(btnAccept);
+		
+		btnCancel = new JButton("Cancelar");
+		btnCancel.setBounds(279, 178, 99, 23);
+		pnlPaymentDebt.add(btnCancel);
+	}
+
+	public JFrame getFrmPaymentDebt() {
+		return frmPaymentDebt;
+	}
+
+	public JPanel getPnlPaymentDebt() {
+		return pnlPaymentDebt;
+	}
+
+	public JTextField getTxtServiceCost() {
+		return txtServiceCost;
+	}
+
+	public JTextField getTxtPendingDebt() {
+		return txtPendingDebt;
+	}
+
+	public JTextField getTxtCurrentDebt() {
+		return txtCurrentDebt;
+	}
+
+	public JButton getBtnAccept() {
+		return btnAccept;
+	}
+
+	public JButton getBtnCancel() {
+		return btnCancel;
+	}
 
 }
--- src/presentacion/vista/PaymentPointsWindow.java
+++ src/presentacion/vista/PaymentPointsWindow.java
@@ -1,5 +1,112 @@
 package presentacion.vista;
 
-public class PaymentPointsWindow {
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class PaymentPointsWindow 
+{
+	private JFrame frmPaymentPoints;
+	private JPanel pnlPaymentPoints;
+	private JLabel lblServiceCost;
+	private JLabel lblPointsUsed;
+	private JTextField txtServiceCost;
+	private JTextField txtPointsUsed;
+	private JButton btnAccept;
+	private JButton btnCancel;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PaymentPointsWindow window = new PaymentPointsWindow();
+					window.frmPaymentPoints.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PaymentPointsWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() 
+	{
+		frmPaymentPoints = new JFrame();
+		frmPaymentPoints.setResizable(false);
+		frmPaymentPoints.setTitle("Payment points");
+		frmPaymentPoints.setBounds(100, 100, 532, 300);
+		frmPaymentPoints.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmPaymentPoints.getContentPane().setLayout(null);
+		
+		pnlPaymentPoints = new JPanel();
+		pnlPaymentPoints.setBounds(10, 11, 506, 249);
+		frmPaymentPoints.getContentPane().add(pnlPaymentPoints);
+		pnlPaymentPoints.setLayout(null);
+		
+		lblServiceCost = new JLabel("Costo de servicio");
+		lblServiceCost.setBounds(20, 23, 110, 23);
+		pnlPaymentPoints.add(lblServiceCost);
+		
+		txtServiceCost = new JTextField();
+		txtServiceCost.setColumns(10);
+		txtServiceCost.setBounds(189, 24, 110, 20);
+		pnlPaymentPoints.add(txtServiceCost);
+		
+		lblPointsUsed = new JLabel("Puntos utilizados");
+		lblPointsUsed.setBounds(20, 60, 110, 23);
+		pnlPaymentPoints.add(lblPointsUsed);
+		
+		txtPointsUsed = new JTextField();
+		txtPointsUsed.setColumns(10);
+		txtPointsUsed.setBounds(189, 61, 110, 20);
+		pnlPaymentPoints.add(txtPointsUsed);
+		
+		btnAccept = new JButton("Acepar");
+		btnAccept.setBounds(96, 178, 89, 23);
+		pnlPaymentPoints.add(btnAccept);
+		
+		btnCancel = new JButton("Cancelar");
+		btnCancel.setBounds(279, 178, 99, 23);
+		pnlPaymentPoints.add(btnCancel);
+	}
+	
+	public JFrame getFrmPaymentPoints() {
+		return frmPaymentPoints;
+	}
+
+	public JPanel getPnlPaymentPoints() {
+		return pnlPaymentPoints;
+	}
+
+	public JTextField getTxtServiceCost() {
+		return txtServiceCost;
+	}
+
+	public JTextField getTxtPointsUsed() {
+		return txtPointsUsed;
+	}
+
+	public JButton getBtnAccept() {
+		return btnAccept;
+	}
+
+	public JButton getBtnCancel() {
+		return btnCancel;
+	}
 
 }
